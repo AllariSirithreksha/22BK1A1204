8d3929d0a10f39419be5b5490a8f48332dedf950
Your Name <you@example.com>
2025-07-22 12:14:17 +0530

    https://github.com/AllariSirithreksha/22BK1A1204

---
+++ react.js
@@ -0,0 +1,11 @@
+
+class App extends React.Component {
+     render(){
+                return (
+                    <div>
+                        <h2>Welcome to My Web App</h2>
+                        <p>This is a simple React component rendered in a web application.</p>
+                        <button onClick={() =>alert()}>Click Me!</button>
+                )
+            }
+        }
